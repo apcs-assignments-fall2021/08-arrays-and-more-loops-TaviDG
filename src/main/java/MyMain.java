3e99586dc80688088d06feb01232142815aceb94
Tavi Greenfield <tavi@elyandpilar.com>
2021-09-24 14:05:50 -0700

    wrote rotate and duplicate code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,22 +5,45 @@ public class MyMain {
     // Rotates all values in an array to the left
     // Ex. rotateLeft([1, 2, 3, 4, 5]) => [2, 3, 4, 5, 1]
     public static int[] rotateLeft(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int[] newArr=new int[arr.length];
+        for (int i =0;i< arr.length;i++){
+            if (!(i==0)){
+                newArr[i-1]=arr[i];
+            }
+            else{
+                newArr[arr.length-1]=arr[i];
+            }
+        }
+        return newArr;
     }
 
     // Rotates all values in an array to the right
     // Ex. rotateRight([1, 2, 3, 4, 5]) => [5, 1, 2, 3, 4]
     public static int[] rotateRight(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int[] newArr=new int[arr.length];
+        for (int i =0;i< arr.length;i++){
+            if (!(i== (arr.length-1))){
+                newArr[i+1]=arr[i];
+            }
+            else{
+                newArr[0]=arr[i];
+            }
+        }
+        return newArr;
     }
 
     // Replaces all duplicate values with -1
     // Ex. duplicateReplacer([1, 2, 1, 2, 1, 1]) => [1, 2, -1, -1, -1, -1]
     public static int[] duplicateReplacer(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        for (int i = 0;i<arr.length;i++){
+            for (int x = 0;x<i;x++){
+                if (arr[x] == arr[i]) {
+                    arr[i] = -1;
+                    break;
+                }
+            }
+        }
+        return arr;
     }
 
 
